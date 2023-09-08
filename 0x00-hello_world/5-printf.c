5284f444142b803eb03630449f4dd61a423be63d
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-07 19:40:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *  main - this is a printing program using printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
