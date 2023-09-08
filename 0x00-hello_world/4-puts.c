b38ea36002d4180db35fe03b44198542df7ecd71
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-07 19:12:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main (void)
+/* main - this is a printing program using puts*/
+int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
