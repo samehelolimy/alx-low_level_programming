51c8322efce686990e95f5c6a4db3269c99060d9
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-09 19:27:16 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ * description: print the alphabet in lowercase
+ * Return: 0 (Successsed)
+*/
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
