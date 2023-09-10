bb6b68188db55e58aab5eeefa42306198d7c651c
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-09 18:53:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ * description: print the alphabet in lowercase
+ * Return: 0 (Successsed)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z' && c != 'q' && c != 'e')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar ('\n');
+	return (0);
+}
