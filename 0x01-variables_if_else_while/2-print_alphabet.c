eba07918ebcce0866b544fd290035714af84ec82
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-09 18:26:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ * description: print the alphabet in lowercase
+ * Return: 0 (Successsed)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	for ( ; c <= z ; c++)
+	putchar(c\n);
+	return (0);
+}
