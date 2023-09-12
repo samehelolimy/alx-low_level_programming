841bcdbe3325d3e7b901efd9e53332da9c88ef83
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-12 15:56:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ * description: print the alphabet in lowercase using putchar
+ * Return: 0 (Successsed)
+*/
+int main(void)
+{
+	putchar(%s, _putchar);
+	return (0);
+}
