a03245c2d359bd1eee0fc03e56e93fcf683f40aa
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-12 17:59:37 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
-
 /**
- * print_alphabet -  printing alphabet in lowercase
+ * print_alphabet_x10 - printing alphabet in lowercase
  * a - z 10 times
 */
-        void print_alphabet_x10(void)
-        {
+	void print_alphabet_x10(void)
+	{
 	int x = 1;
-	
+
 	while (x <= 10)
 	{
-        char c = 'a';
+	char c = 'a';
 
-        while (c <= 'z')
-        {
-        putchar(c);
-        c++;
-        }
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
 	x++;
 	}
-        putchar('\n');
-        }
+	putchar('\n');
+	}
