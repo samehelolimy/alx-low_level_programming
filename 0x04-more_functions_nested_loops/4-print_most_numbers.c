02681d8444c48dc12743d611a5cf56a320b0ff6d
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-14 09:16:33 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *description: print_most_numbers - prints from 0 to 9
+*/
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57 && i != 50 && i != 52)
+	{
+	putchar(i);
+	i++;
+	}
+	putchar('\n');
+}
