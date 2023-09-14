8197191a9f9598fcb5cd1a5d2fccc510a4115c87
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-14 08:40:42 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- *description:_isdigit  -  checks if digit from 0 to 9
- * Return: 1 if true 0 if false
- * @c: our parameter
+ *description: mul - multiplies two integers
+ * @a: our first parameter
+ * @b: second parameter
+ * Return: z our result
 */
 int mul(int a, int b)
 {
 	int z;
+
 	z = a * b;
 	return (z);
 	putchar('\n');
