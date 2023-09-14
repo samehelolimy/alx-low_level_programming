3265900363fa1daed4a2e9c217950cae83108cd6
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-14 08:31:18 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *description:_isdigit  -  checks if digit from 0 to 9
+ * Return: 1 if true 0 if false
+ * @c: our parameter
+*/
+int mul(int a, int b)
+{
+	int z;
+	z = a * b;
+	return (z);
+	putchar('\n');
+}
