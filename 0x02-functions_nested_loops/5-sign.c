9e03188d1ec33d1b65f4c17e9649f4cd3a8ef2fe
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-15 16:34:20 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - our function
+ * Description:  print_sign - checking number + or - or 0
+ * Return: 1 if positive ,return -1 if negative , return 0 if 0
+ * @n: our parameter
+*/
+	int print_sign(int n);
+	{
+	if (n > 0)
+	putchar (43);
+	return (1);
+	else if (n < 0)
+	putchar (45);
+	return (-1);
+	else
+	putchar (48);
+	return (0);
+	return (0);
+	}
