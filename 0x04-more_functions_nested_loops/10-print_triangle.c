c71ae9d87fd8dd3b8095d7e46c1d01a64d51d412
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-15 04:47:10 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle -  printing a triangle
+ * @size: our parameter
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	putchar('\n');
+	else
+	{
+	for (i = 1 ; i <= size ; i++)
+	{
+	for (j = 0; j <= i ; j++)
+	putchar('#');
+	putchar('\n');
+	}
+	}
+}
