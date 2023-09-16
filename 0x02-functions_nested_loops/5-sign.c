83c1a96f9c90a7d871884772b9973f9bd9f3ff24
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-15 17:03:25 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,21 @@
  * Return: 1 if positive ,return -1 if negative , return 0 if 0
  * @n: our parameter
 */
-	int print_sign(int n)
-	{
+int print_sign(int n)
+{
 	if (n > 0)
 	{
 	putchar (43);
 	return (1);
 	}
 	else if (n == 0)
-        {
-        putchar (48);
-        return (0);
+	{
+	putchar (48);
+	return (0);
 	}
 	else if (n < 0)
 	{
 	putchar (45);
 	return (-1);
 	}
-	}
+}
