f4362d85e6c05770d90c1e7e8dd6b2d0bab275bc
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-17 07:25:31 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ *Description: main  - entry point
+ *Return: 0 Success
+*/
+int main(void)
+{
+	for (i = 0 ; i <= 100 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+		putchar('F');
+		putchar('i');
+		putchar('z');
+		putchar('z');
+		{
+		else if (i % 5 == 0)
+		{
+		putchar('B');
+		putchar('u');
+		putchar('z');
+		putchar('z');
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+		putchar('F');
+		putchar('i');
+		putchar('z');
+		putchar('z');
+		putchar('B');
+		putchar('u');
+		putchar('z');
+		putchar('z');
+		}
+		else
+		{
+		putchar(i);
+		}
+		putchar(' ');
+		}
+}
