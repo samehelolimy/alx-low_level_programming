14310c4a8bf57b79897779844de858f325110045
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-17 06:37:05 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - our function
+ * Description: adding two integrs
+ * Return: result
+ * @x: first parameter
+ * @y: second parameter
+*/
+int add(int x, int y)
+{
+        int result;
+	result = x +y ;
+	return (result);
+}
