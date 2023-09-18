43496d1df37853b4591ef16a5b27f9a6fad0e537
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-18 13:22:27 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap values of two integers
+ * @a: first parameter
+ * @b: second parameter
+*/
+void swap_int(int *a, int *b)
+{
+	int x;
+	*b = *a;
+	*a = x;
+	x = *b;
+}
+
