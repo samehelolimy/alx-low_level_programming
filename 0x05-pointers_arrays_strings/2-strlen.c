76da0b0e87cd01e85bc3dd251eb999df688fbb60
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-18 14:00:56 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: is our parameter
+ * Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; ++i)
+	return (i);
+}
