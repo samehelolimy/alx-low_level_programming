4018c0fd0fb0e54171512fc479472ac84deb7b06
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-18 16:56:46 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+/**
+ * rev_string - reverse a string
+ * @s: - our parameter
+*/
+#include "main.h"
+void rev_string(char *s)
+{
+	int i;
+
+	while (s[i])
+	{
+	i++;
+	while (i--;)
+	return (s[i]);
+	}
+}
