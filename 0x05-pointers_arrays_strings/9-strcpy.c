cc50bed01661f52d238fab174befbe6591d78d04
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-19 14:56:30 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,9 +3,10 @@
  * _strcpy - copies the string pointed to by src
  * @dest: first parameter
  * @src: second parameter
+ * Return: copy value
 */
 char *_strcpy(char *dest, char *src)
 {
 	*src = *dest
-	return *dest;
+	return (*dest);
 }
