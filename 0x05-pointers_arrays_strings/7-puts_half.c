1f5e7a336ae024adf8be56f5eacfce5510efcaad
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-19 18:19:51 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,12 @@ void puts_half(char *str)
 	int i;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
+	{
 		i++;
 		for (i /= 2 ; str[i] != '\0' ; i++)
 		{
 		printf("%c", str[i]);
 		}
 }
-	printf("%C", "\n");
+	printf("%C", '\n');
 }
