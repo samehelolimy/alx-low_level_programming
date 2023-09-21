0f4b36fb341e70d13995d4e3709efa5ba6c07c56
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-21 09:17:15 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,15 +7,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
 	while (dest[i] != '\0')
 	i++;
-	j = 0;
 
 	while (src[j] != '\0')
 	{
-	dest[++i] = src[j++];
+	dest[i] = src[j];
+	j++;
+	i++;
 	}
 	dest[i] = '\0';
 	return (dest);
