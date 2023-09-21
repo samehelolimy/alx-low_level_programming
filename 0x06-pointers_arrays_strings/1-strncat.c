da65a90d789139ae7858ab274e6db12341c0156a
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-21 10:32:45 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 	i++;
-	while (j < n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
 
