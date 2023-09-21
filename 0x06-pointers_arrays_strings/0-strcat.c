0a2d71731129ff173fef6c27528470a0d5c1f1e2
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-21 04:07:40 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcat - concatenating two strings
+ *@dest: first parameter
+ *@src: second parameter
+ *Return: *dest success
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	while (dest[i] < '\0')
+	{
+	i++;
+	}
+	j = 0;
+
+	while (src[j] < '\0')
+	{
+	dest[i] = dest[i] + src[j]
+	j++;
+	return (*dest);
+	}
+	putchar('\n');
+}
