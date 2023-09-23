edf121acb0cdd61caa44aa5a8fa445f9fafeb490
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-22 19:16:28 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	int n = 0;
 
-	while (dest[i] != '\0' && src[n] != '\0')
+	while (src[j] != '\0')
+	{
 	dest[i] = src[j]
-	i++;
 	j++;
 	}
+	if (dest[i] == '\0'
+
 	return (dest);
 }
 
