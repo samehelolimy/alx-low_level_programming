56fa932e83016d7f40ace48dd5189737621f8184
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-23 03:57:14 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses an array
+ * @a: first parameter
+ * @n: second parameter
+ * Return: reversed array
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	while (a[i])
+		i++;
+	if (a[i] != '\0' && i != n)
+	{
+		printf(a[i]);
+	}
+	a[i] == '\0';
+}
+return (a);
+
