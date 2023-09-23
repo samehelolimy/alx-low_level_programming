7258595cdd64d6c05c07982ff980d6e0ad3b951f
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-23 10:23:45 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,11 @@ void reverse_array(int *a, int n)
 {
 	int i, j, s;
 
-	for(i = 0 ; j = n-1 ; i < j ; i++ ; j--)
+	for (i = 0, j = n - 1 ; i < j ; i++, j--)
 	{
 	s = a[i];
 	a[i] = a[j];
 	a[j] = s;
 	}
-	return (a);
 }
 
