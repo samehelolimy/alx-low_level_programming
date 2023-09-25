7bc02e272d1996ced5d3062b4df2d8ec2ba1af67
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-25 15:35:08 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,11 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *s;
+	unsigned int i;
 
-	s = &n;
-	int i;
-
-	for (i = 0 ; i <= n ; i++)
+	for (i = 0 ; n > 0 ; i++, n--); 
 	{
-		*s = b;
+		s[i]= b;
 	}
 	return (s);
 }
