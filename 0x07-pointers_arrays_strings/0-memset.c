dbcae604834665c0d5c59cc4db8d9d93581ba4a8
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-25 15:09:54 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: first parameter
+ * @b: second parameter
+ * @n: third one
+ * Return: success
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *s;
+	s = &n;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		*s = b;
+	}
+	return (s);
+}
+	
