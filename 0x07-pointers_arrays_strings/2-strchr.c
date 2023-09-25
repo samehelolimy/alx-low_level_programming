8166a2a64cf24c75c4ad8f97f410e5d5aec9da1e
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-25 15:57:20 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - locates a char in a string
+ * @s: first parameter
+ * @c: second parameter
+ * Return: success
+*/
+char *_strchr(char *s, char c)
+{
+	for (i = ; s[i] < '\0'; i++)
+	{
+		if (s[i] == 'c')
+			return (s);
+		else
+			return (0);
+	}
+}
