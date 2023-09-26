b1a5cc73b65ba8bf78f2a5f9a435e828db7e07cb
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-26 01:59:13 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,9 +9,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; i < '\0'; i++)
+	for (i = 0; i != '\0'; i++)
 	{
-		for (j = 0; j < '\0'; j++)
+		for (j = 0; j != '\0'; j++)
 		{
 		if (s[i] == accept[j])
 		{
