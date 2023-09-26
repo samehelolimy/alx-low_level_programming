f60f8ef11f372cd87543d5774e9e7594b3fb28f5
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-26 11:11:43 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,16 +7,16 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
+	unsigned int i, j;
 
 	for (i = 0 ; s[i] != '\0'; i++)
 	{
 		for (int j = 0 ; accept[j] != s[i] ; j++)
 		{
 			if (accept[j] == '\0')
-				return (i);
+			return (i);
 		}
 	}
-		return (i);
+	return (i);
 }
 
