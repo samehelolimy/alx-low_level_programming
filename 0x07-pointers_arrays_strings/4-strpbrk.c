1191e19678643ebc8224e259d0e7f05a86edcc5f
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-26 01:56:06 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - searches a string
+ * @s: first parameter
+ * @accept: second parameter
+ * Return: success
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; i < '\0'; i++)
+	{
+		for (j = 0; j < '\0'; j++)
+		{
+		if (s[i] == accept[j])
+		{
+		return (s);
+		}
+		}
+	}
+		return ('\0');
+}
