284872a3618ca6a7fbb4362cf63cfabb1b538059
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-26 08:15:45 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strstr - locates asubstring
+ * @haystack: first parameter
+ * @needle: second parameter
+ * Return: success
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	
+       for (i = 0 ; i != '\0'; i++)
+       {
+	       for (j = 0 ; j != '\0' ; j++)
+	       if (needle[j] == haystack[i])
+	       {
+		       return (&needle[j]);
+	       }
+	       return ('\0');
+       }
+}
+
