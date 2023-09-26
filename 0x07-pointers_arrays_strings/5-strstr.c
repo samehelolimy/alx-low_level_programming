a446db871f49bc4fe74c371abeee11be4a0b8b40
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-26 08:20:44 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,15 +8,17 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
-	
-       for (i = 0 ; i != '\0'; i++)
-       {
-	       for (j = 0 ; j != '\0' ; j++)
-	       if (needle[j] == haystack[i])
-	       {
-		       return (&needle[j]);
-	       }
-	       return ('\0');
-       }
+
+	for (i = 0 ; i != '\0'; i++)
+	{
+		for (j = 0 ; j != '\0' ; j++)
+		{
+		if (needle[j] == haystack[i])
+		{
+		return (&needle[j]);
+		}
+		}
+		return ('\0');
+	}
 }
 
