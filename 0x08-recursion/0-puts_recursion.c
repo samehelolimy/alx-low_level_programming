17982ba8ed77891fca4663a89480c53a8708e575
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-30 07:34:36 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: our parameter
+ * Return: success
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	putchar(_puts_recursion(char *s));
+}
