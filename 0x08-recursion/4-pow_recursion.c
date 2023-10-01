83bcda6a8f6e149803ba13367e02052b564f8270
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-01 06:45:07 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,11 +8,17 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1);
+	{
+	return (-1);
+	}
 	if (y == 0)
-		return (1);
+	{
+	return (1);
+	}
 	if (y == 1)
-		return (x);
+	{
+	return (x);
+	}
 	x = (x * x) * (y - 1);
 	_pow_recursion(x, y);
 	return (x);
