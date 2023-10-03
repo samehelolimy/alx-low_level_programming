1d4f72b31695d1bf2533c64a593e70daeb81c0d7
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-03 16:23:26 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,13 +29,13 @@ char *str_concat(char *s1, char *s2)
 		s1 = "\0";
 	if (s2 == NULL)
 		s2 = "\0";
+
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
-	{
 	return (0);
-	}
+
 	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
