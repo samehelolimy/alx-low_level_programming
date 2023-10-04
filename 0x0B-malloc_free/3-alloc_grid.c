81de62621eb4504731cafe119e31fece3d9811f1
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-04 03:53:46 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,11 +9,12 @@ int **alloc_grid(int width, int height)
 {
 	if (width == 0 || height == 0)
 		return (NULL);
-	int i, j;
-	char *arr;
+	int i = 0;
+	int j = 0;
+	char **arr;
 
-	for (i = 0; arr[i][j] != '\0';)
+	for (; arr[i][j] != '\0';)
 	{
-		return (*arr);
+		return (**arr);
 	}
 }
