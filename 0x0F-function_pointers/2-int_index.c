a92d2fade0febf271b5472196a175aaef9354269
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-12 07:27:17 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: first parameter
+ * @size: second parameter
+ * @cmp: function parameter
+ * Return: success
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]);
+			return (i);
+			i++;
+		}
+	return (-1);
+
+}
