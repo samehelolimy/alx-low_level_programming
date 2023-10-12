2955d5479458d5bab4250266fb4459cbbdb41e76
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-12 10:14:39 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - add function
+ * @a: first par
+ * @b: sec par
+ * Return: result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub function
+ * @a: first par
+ * @b: sec par
+ * Return: result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul function
+ * @a: first par
+ * @b: sec par
+ * Return: result
+*/
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_dev - dev function
+ * @a: first par
+ * @b: sec par
+ * Return: result
+*/
+
+int op_dev(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - mod function
+ * @a: first par
+ * @b: sec par
+ * Return: result
+*/
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
