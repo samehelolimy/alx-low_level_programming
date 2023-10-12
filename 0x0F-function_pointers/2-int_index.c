c65a35cde8968ba4fe5cd8d22e47d26c5f1d96f3
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-12 07:34:03 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && size && cmp)
 		while (i < size)
 		{
-			if (cmp(array[i]));
+			if (cmp(array[i]))
 			return (i);
 			i++;
 		}
