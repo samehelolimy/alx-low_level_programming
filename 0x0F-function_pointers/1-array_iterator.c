fff4ffdd6f9e95c8bccdb202b4f9e976cd6fe327
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-12 06:08:42 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * @array: first
+ * @size: sec
+ * @action: function parameter
+ * Return: success
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (for i = 0 ; array[i] != '\0' ; i++)
+	{
+		action[*array]
+	}
