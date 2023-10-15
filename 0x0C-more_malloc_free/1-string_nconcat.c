486792f805e6664bab5e293f7c28b35f4c1ac81f
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-15 09:40:37 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-		;
+		
 		str = malloc(s1_length + n + 1);
 	if (str == NULL)
 	{
