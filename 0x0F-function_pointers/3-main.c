cea8be2a80943c82eed811c9620777530488502f
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-15 16:49:19 -0700

    3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	printf("Error\n"), exit(98);
 
-	a = atoi(argv[1];
-	b = atoi(argv[3];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 		op_func = get_op_func(argv[2]);
 		if (!op_func)
