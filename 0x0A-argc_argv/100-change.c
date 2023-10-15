02bf8aaf52547714060c33e70029973a50dc848e
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-15 14:33:19 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints minimum
+ * @argc: no og command line arg
+ * @argv: pointer to an array of c l
+ * Return:success
+ */
+int main(argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(argv[1]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
