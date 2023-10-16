5923c098074f3fc64321a3fe2c84678aceedb844
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-15 17:01:47 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include "function_parameters.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code
+ * @argc: the no of args
+ * @argv: arg pointer
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
