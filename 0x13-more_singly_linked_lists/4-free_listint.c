9f78515926fa26439758d31bfda6543863601434
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-24 16:17:12 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: our pointer
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+	tmp = head->next;
+	free(head);
+	head = tmp;
+	}
+}
+
