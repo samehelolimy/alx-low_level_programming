c96a0bc97ed9e6c28a8400450f46f1211affd887
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-24 04:14:51 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "list.h"
+/**
+ * print_listint - our function
+ * @h : points to next element
+ * Return: list element
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t x = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
+
