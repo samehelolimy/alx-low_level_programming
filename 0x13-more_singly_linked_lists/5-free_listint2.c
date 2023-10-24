aab88e20dc298efcff0092b72d5c3353c050b394
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-24 16:27:20 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: our pointer
+ * Return: success
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *node, *tmp;
+
+if (!head)
+	return;
+	node = *head;
+	while (node)
+	{
+		tmp = node;
+		node = node->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
+
