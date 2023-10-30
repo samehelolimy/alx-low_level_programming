0aa5ce631905b6116b33193c64541040575b00f2
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-30 12:37:45 -0700

    0-read_textfile.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd>
-#include <fcntl>
+#include <unistd.h>
+#include <fcntl.h>
+
 #define READ_BUF_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
