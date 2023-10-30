79bec0173980b4febe921ebf71ddd844b116ed1b
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-30 14:03:26 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSER | S_IWUSR);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
 	if (l)
