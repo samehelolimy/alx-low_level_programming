831f404b7670daf9d6774fb427e65e6a2f346b3e
sameh.elolimy <sameh.elolimy@gmail.com>
2023-10-30 12:34:39 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	f = open(filename, O_RDONLY);
 	if (f == -1)
 		return (0);
 	byte = read(f, &buf[0], letters);
