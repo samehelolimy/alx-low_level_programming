107ef68c6ca4d6986369aad318ce4f32303e0a62
samehmohamed <sameh.elolimy@gmail.com>
2023-11-13 13:58:48 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -38,5 +38,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (l)
 	byte = write(f, text_content, l);
 	close(f);
-	return (byte == l ? 1 : 1);
+	return (byte == l ? 1 : -1);
 }
