ba1cd6a7549b5f9e4c06cb9d25fedb601b50b42e
samehmohamed <sameh.elolimy@gmail.com>
2023-11-13 13:55:22 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,35 +8,35 @@
 
 int _strlength(char *x)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!x)
-                return (0);
-        while (*x++)
-                i++;
-        return (i);
+	if (!x)
+	return (0);
+	while (*x++)
+	i++;
+	return (i);
 }
 
 /**
- * create_file - creates a file
- * @filename: name of file
- * @text_content: text
+ * append_text_to_file - appends text at the end of a file
+ * @filename: first par
+ * @text_content: sec par
  * Return: success
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-        int f;
-        ssize_t byte = 0;
-        ssize_t l = _strlength(text_content);
+	int f;
+	ssize_t byte = 0;
+	ssize_t l = _strlength(text_content);
 
-        if (!filename)
-                return (-1);
-        f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-        if (f == -1)
-                return (-1);
-        if (l)
-                byte = write(f, text_content, l);
-        close(f);
-        return (byte == l ? 1 : 1);
+	if (!filename)
+	return (-1);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
+	return (-1);
+	if (l)
+	byte = write(f, text_content, l);
+	close(f);
+	return (byte == l ? 1 : 1);
 }
