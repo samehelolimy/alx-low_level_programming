c479388c6c10f2528b2c65e541d63309532d6abd
samehmohamed <sameh.elolimy@gmail.com>
2023-11-13 14:02:10 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,7 +32,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 	return (-1);
-	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	f = open(filename, O_WRONLY | O_APPEND);
 	if (f == -1)
 	return (-1);
 	if (l)
